3fd393ef0226163c9538704106762885b505d93b
chickenjdev <tienkhoiqnk99@gmail.com>
2020-05-05 21:40:05 +0700

    git test change

--- app/src/main/java/com/example/diemdanhapp/MainActivity.java
+++ app/src/main/java/com/example/diemdanhapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.diemdanhapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // test phat ne
+        Log.i("MainActivity","test thu cai");
     }
 }
+
