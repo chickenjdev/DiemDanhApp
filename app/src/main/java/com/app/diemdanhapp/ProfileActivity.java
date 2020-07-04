cdb3b5dfdbf7f19b86efa29988374bfc4519fc10
ngoclong3601 <prolavip905@gmail.com>
2020-07-04 21:57:38 +0700

    forgot password, gif,...

--- app/src/main/java/com/app/diemdanhapp/ProfileActivity.java
+++ app/src/main/java/com/app/diemdanhapp/ProfileActivity.java
@@ -6,13 +6,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import com.app.model.loginModel;
 import com.app.model.userInfo;
 
 
 public class ProfileActivity  extends Activity{
     TextView txtFullname;
+    EditText etxtPhone;
+    EditText etxtAccommodation;
+    Button btnUpdate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +26,16 @@ public class ProfileActivity  extends Activity{
         txtFullname = (TextView)findViewById(R.id.txtFullname);
 
         txtFullname.setText(userInfo.getFull_name());
+        btnUpdate.setOnClickListener( new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                etxtPhone = (EditText) findViewById(R.id.txtPhone);
+                etxtPhone.setEnabled(true);
+                etxtAccommodation= (EditText) findViewById(R.id.txtAccommodation);
+                etxtAccommodation.setEnabled(true);
+
+            }
+        });
 
     }   @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
